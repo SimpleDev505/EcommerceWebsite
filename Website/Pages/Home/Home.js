a78e354db78dcc105cd555fdd323153f249c4c05
SimpleDev505 <114297867+SimpleDev505@users.noreply.github.com>
2024-07-18 20:38:26 +0530

    restructure

--- Website/Pages/Home/Home.js
+++ Website/Pages/Home/Home.js
@@ -11,7 +11,17 @@ var MainImage_Collection_PositionX = ["right",
    "left",
    "right"
 ]
+const TitleText = document.getElementById("TitleText");
+const TitleText_PositionX = ["left","right","left"];
+var TitleText_Collection = ["Unbeatable Deals, Just for You! Shop Now and Save Big!",
+                            "Innovative Essentials: Redefine Your Everyday with Our Bestsellers!",
+                            "Unlock Exclusive Benefits: Members-Only Rewards Await!"
+                           ]
+var Middle_Section = document.getElementById("Middle");
+var Middle_Section_Color = ["#f5d55a","#78D3FD","#C4B3F2"];
 var MainImage_index = 0;
+
+
 function updateMainImage(){
    // MainImage.style = MainImage_Style;
    MainImage.animate([{opacity:0
@@ -19,10 +29,17 @@ function updateMainImage(){
                      ,{opacity:1
                      }]
                      ,1000);
+   TitleText.innerHTML = ""+TitleText_Collection[MainImage_index] ;
+   TitleText.parentElement.style.float = TitleText_PositionX[MainImage_index];
+
+   Middle_Section.style.backgroundColor = Middle_Section_Color[MainImage_index];
    MainImage.style.background = "url("+MainImage_Collection[MainImage_index]+") no-repeat "+MainImage_Collection_PositionX[MainImage_index];
    MainImage.style.backgroundSize = MainImage_Collection_Size[MainImage_index];
    MainImage_index = (MainImage_index +1) % MainImage_Collection.length;
    console.warn(MainImage_index);
 }
 setInterval(updateMainImage,3000);
+
+
+
 // .style.background = "url('/Website/Images/MainDisplay/Main1_1.png') no-repeat";
